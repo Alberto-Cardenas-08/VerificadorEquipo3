e5127b81577dbb16180dfdefa04477e4c55661cd
Alberto Cardenas <al05059229@tecmilenio.mx>
2026-03-25 15:09:39 -0500

    Parte 1 y 2

--- VerificadorEquipo3.py
+++ VerificadorEquipo3.py
@@ -1,1 +1,12 @@
-print("holamundo")
+Equipo = "Lechugas" 
+integrantes = ["Raymundo", "Alberto", "Sebastian", "Jared", "Brenda"]
+print("El equipo es:", Equipo)
+print("Los integrantes del equipo son:")
+for integrante in integrantes:
+    print("-", integrante)
+
+contraseñas = ["Uva2", "Komala1970", "Fulljosh", "Palpit0ad", "Gwenely17", "Desklewis", "parissesw", "Lolo", "Tulio31", "Leche"] 
+for contraseña in contraseñas:
+    print("Contraseña:", contraseña)
+
+contrasena
