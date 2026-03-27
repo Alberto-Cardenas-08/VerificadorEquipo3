18e094f787c298f918c64b0a93d7da172f96236b
Jared <lichfoxyking@gmail.com>
2026-03-26 23:49:00 -0500

    Todo débil bug fixed

--- VerificadorEquipo3.py
+++ VerificadorEquipo3.py
@@ -5,21 +5,21 @@ print("El equipo es:", Equipo)
 print("Los integrantes del equipo son:")
 for integrante in integrantes:
     print("-", integrante)
+
 def verificar(contrasena):
     if len(contrasena) < 6:
         return "Débil"
-    elif not any(c.isdigit() for c in contrasena):
+    elif len(contrasena) >= 6 and not any(c.isdigit() for c in contrasena):
         return "Media"
     else:
         return "Fuerte"
 
+contraseñas = ["Uva2", "Komala1970", "Fulljosh", "Palpit0ad", "Gwenely17", "Desklewis", "parissesw", "Lolo", "Tulio31", "Leche"] 
 
-contraseñas = ["Uva2", "Komala1970", "Fulljosh", "Palpit0ad", "Gwenely17", "Desklewis", "parissesw", "Lolo", "Tulio31", "Leche"]
 print("Lista de contraseñas:")
-for c in contraseñas:
-    print("-", c)
-    
-print("\nResultados de la verificación de contraseñas:")    
-for contrasena in contraseñas:
-    resultado = verificar([contrasena])
-    print(f"La contraseña '{contrasena}' es: {resultado}")
+for i, c in enumerate(contraseñas, 1):
+    print(f"{i}. {c}")
+
+for contraseña in contraseñas:
+    resultado = verificar(contraseña)
+    print("Contraseña:", contraseña, "-", resultado)
