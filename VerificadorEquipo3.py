67e7e559f659cda9164dd9e8f95c494c9981c553
Alberto-Cardenas-08 <al05059229@tecmilenio.mx>
2026-03-26 23:24:52 -0500

    Lo mismo pero arreglado

--- VerificadorEquipo3.py
+++ VerificadorEquipo3.py
@@ -1,23 +1,21 @@
 Equipo = "Lechugas" 
 integrantes = ["Raymundo", "Alberto", "Sebastian", "Jared", "Brenda"]
 
+print("El equipo es:", Equipo)
 print("Los integrantes del equipo son:")
 for integrante in integrantes:
     print("-", integrante)
 
 contraseñas = ["Uva2", "Komala1970", "Fulljosh", "Palpit0ad", "Gwenely17", "Desklewis", "parissesw", "Lolo", "Tulio31", "Leche"] 
 
-for contraseña in contraseñas:
-    resultado = verificar(contraseñsa)
-    print("Contraseña:", contraseña, "-", resultado)
-
-def verificar(contrasena):
-    if len(contrasena) < 6:
-        return "Débil"
-    elif len(contrasena) >= 6 and not any(c.isdigit() for c in contrasena):
-        return "Media"
-    else:
-        return "Fuerte"
+def verificar(contraseñas):
+    for contrasena in contraseñas:
+        if len(contrasena) < 6:
+            return "Débil"
+        elif len(contrasena) >= 6 and not any(c.isdigit() for c in contrasena):
+            return "Media"
+        else:
+            return "Fuerte"
 
 print("\nResultados de la verificación de contraseñas:")        
 for contrasena in contraseñas:
