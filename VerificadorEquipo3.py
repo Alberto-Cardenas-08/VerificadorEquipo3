c693dcae8b5fc35b27b8f431536e207e0fde4150
Jared <lichfoxyking@gmail.com>
2026-03-26 22:59:33 -0500

    Se arregló error de por definición de Verificación

--- VerificadorEquipo3.py
+++ VerificadorEquipo3.py
@@ -1,19 +1,23 @@
 Equipo = "Lechugas" 
 integrantes = ["Raymundo", "Alberto", "Sebastian", "Jared", "Brenda"]
+
 print("El equipo es:", Equipo)
 print("Los integrantes del equipo son:")
 for integrante in integrantes:
     print("-", integrante)
 
-contraseñas = ["Uva2", "Komala1970", "Fulljosh", "Palpit0ad", "Gwenely17", "Desklewis", "parissesw", "Lolo", "Tulio31", "Leche"] 
-for contraseña in contraseñas:
-    resultado = verificar(contraseñsa)
-    print("Contraseña:", contraseña, "-", resultado)
 
 def verificar(contrasena):
     if len(contrasena) < 6:
         return "Débil"
-    elif len(contrasena) >= 6 and not any(c.isdigit() for c in contrasena):
+    elif not any(c.isdigit() for c in contrasena):
         return "Media"
     else:
         return "Fuerte"
+
+
+contraseñas = ["Uva2", "Komala1970", "Fulljosh", "Palpit0ad", "Gwenely17", "Desklewis", "parissesw", "Lolo", "Tulio31", "Leche"]
+
+for contraseña in contraseñas:
+    resultado = verificar(contraseña)
+    print("Contraseña:", contraseña, "-", resultado)
