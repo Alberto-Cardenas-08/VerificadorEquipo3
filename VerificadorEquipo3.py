bbdea897741bd4b3cbb598cd14801f4604378855
firefoot <firefotroden@gmail.com>
2026-03-26 23:06:07 -0500

    Arregle unos fallos de sintaxis y de print

--- VerificadorEquipo3.py
+++ VerificadorEquipo3.py
@@ -1,11 +1,16 @@
 Equipo = "Lechugas" 
 integrantes = ["Raymundo", "Alberto", "Sebastian", "Jared", "Brenda"]
+<<<<<<< HEAD
+=======
+
+>>>>>>> b484dde (Arregle unos fallos de sintaxis y de print)
 print("El equipo es:", Equipo)
 print("Los integrantes del equipo son:")
 for integrante in integrantes:
     print("-", integrante)
 
 contraseñas = ["Uva2", "Komala1970", "Fulljosh", "Palpit0ad", "Gwenely17", "Desklewis", "parissesw", "Lolo", "Tulio31", "Leche"] 
+<<<<<<< HEAD
 for contraseña in contraseñas:
     resultado = verificar(contraseñsa)
     print("Contraseña:", contraseña, "-", resultado)
@@ -17,3 +22,19 @@ def verificar(contrasena):
         return "Media"
     else:
         return "Fuerte"
+=======
+
+def verificar(contraseñas):
+    for contrasena in contraseñas:
+        if len(contrasena) < 6:
+            return "Débil"
+        elif len(contrasena) >= 6 and not any(c.isdigit() for c in contrasena):
+            return "Media"
+        else:
+            return "Fuerte"
+
+print("\nResultados de la verificación de contraseñas:")        
+for contrasena in contraseñas:
+    resultado = verificar([contrasena])
+    print(f"La contraseña '{contrasena}' es: {resultado}")
+>>>>>>> b484dde (Arregle unos fallos de sintaxis y de print)
